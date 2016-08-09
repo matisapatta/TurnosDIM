d5cd50926660fb532befd40c3a42ee16e2a88466
matisapatta <mati.sapatta@gmail.com>
2016-08-09 15:26:45 -0300

    added drawer

--- app/src/main/java/mobile/mads/turnosdim/EntryActivity.java
+++ app/src/main/java/mobile/mads/turnosdim/EntryActivity.java
@@ -1,13 +1,32 @@
 package mobile.mads.turnosdim;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class EntryActivity extends AppCompatActivity {
+
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_entry);
+        loginBtn=(Button)findViewById(R.id.loginBtn);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        loginBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(EntryActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
