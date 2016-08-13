7b6f64e897122aa864684c096cbb181b8b176097
matisapatta <mati.sapatta@gmail.com>
2016-08-13 16:22:07 -0300

    Turnos Fragment

--- app/src/main/java/layout/TurnosFragment.java
+++ app/src/main/java/layout/TurnosFragment.java
@@ -7,6 +7,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import mobile.mads.turnosdim.R;
 
@@ -23,6 +25,7 @@ public class TurnosFragment extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+    private Spinner spinnerEspecialidad;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -59,13 +62,24 @@ public class TurnosFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_turnos, container, false);
+        View view = inflater.inflate(R.layout.fragment_turnos, container, false);
+        spinnerEspecialidad = (Spinner)view.findViewById(R.id.spinnerEspecialidades);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.especialidadesArray, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinnerEspecialidad.setAdapter(adapter);
+
+
+        return view;
     }
 
     // TODO: Rename method, update argument and hook method into UI event
