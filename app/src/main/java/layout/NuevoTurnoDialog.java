98b9a8b61dc4e01c37483c71a1f45fe64f523936
matisapatta <mati.sapatta@gmail.com>
2016-08-26 13:36:04 -0300

    dialogo nuevo turno

--- app/src/main/java/layout/NuevoTurnoDialog.java
+++ app/src/main/java/layout/NuevoTurnoDialog.java
@@ -9,12 +9,17 @@ import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.Date;
+
 import mobile.mads.turnosdim.R;
 import mobile.mads.turnosdim.TurnosStruct;
+import mobile.mads.turnosdim.Util;
 
 /**
  * Created by mati on 8/25/16.
@@ -56,10 +61,28 @@ public class NuevoTurnoDialog extends android.support.v4.app.DialogFragment {
         LayoutInflater inflater = getActivity().getLayoutInflater();
         View v = inflater.inflate(R.layout.dialog_nueva_consulta,null);
         builder.setView(v);
-        TextView text = (TextView)v.findViewById(R.id.lblMedicoDialogo);
-        text.setText(turno.getHoraTurno());
-        builder.setMessage(turno.getMedico())
-                .setTitle(R.string.nuevaConsulta)
+        TextView textDia = (TextView)v.findViewById(R.id.lblDiaDialogo);
+        TextView textNumDia = (TextView)v.findViewById(R.id.lblNumDiaDialogo);
+        TextView textMes = (TextView)v.findViewById(R.id.lblMesDialogo);
+        final TextView textMed = (TextView)v.findViewById(R.id.lblMedicoDialogo);
+        TextView textEsp = (TextView)v.findViewById(R.id.lblEspecialidadDialogo);
+        TextView textHora = (TextView)v.findViewById(R.id.lblHoraDialogo);
+        final Button btnAceptar = (Button)v.findViewById(R.id.btnAceptar);
+        Button btnCancelar = (Button)v.findViewById(R.id.btnCancelar);
+
+        Date date = new Util().StringToDate(turno.getFechaTurno());
+        String day = new Util().getDay(date);
+        String dayn = new Util().getNumberDay(date);
+        String month = new Util().getMonth(date);
+        textNumDia.setText(dayn);
+        textDia.setText(day);
+        textMes.setText(month);
+        textMed.setText(turno.getMedico());
+        textEsp.setText(turno.getEspecialidad());
+        textHora.setText(turno.getHoraTurno());
+
+
+        builder.setTitle(R.string.nuevaConsulta)
                 .setPositiveButton("SI", new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int id) {
                         // FIRE ZE MISSILES!
@@ -71,7 +94,23 @@ public class NuevoTurnoDialog extends android.support.v4.app.DialogFragment {
                     }
                 });
         // Create the AlertDialog object and return it
-        return builder.create();
+        final AlertDialog dialog =  builder.create();
+
+        btnAceptar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+
+        btnCancelar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textMed.setText("HOLA");
+            }
+        });
+
+        return dialog;
     }
 
 }
